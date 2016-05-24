c4db8feb803110b13ed6fa19c7f4a09aabf73949
Andrew Carmer <carmerandrew@gmail.com>
2016-05-24 09:42:50 -0600

    ready for many-to-many-part2 challenge

--- db/migrate/20160524035046_create_postal_codes.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostalCodes < ActiveRecord::Migration
-  def change
-    create_table :postal_codes do |t|
-      t.string :zipcode
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20160524045312_remove_column_reports_to_from_employees.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnReportsToFromEmployees < ActiveRecord::Migration
-  def change
-    remove_column :employees, :reports_to, :integer
-  end
-end
